28f5952db59255d315840b4c254cc16e0d6d93b9
Wesley Zhao <wesley.zhao@gmail.com>
2011-12-12 20:14:01 -0800

    like buttons on ad pages should also change to me gusta meow

--- content_script.js
+++ content_script.js
@@ -1,5 +1,6 @@
 var oLink = document.createElement('link');
-oLink.href = 'https://dl.dropbox.com/u/19699329/megusta.css';
+if (isSecure()) oLink.href = 'https://dl.dropbox.com/u/19699329/megusta.css';
+else oLink.href = 'http://dl.dropbox.com/u/19699329/megusta.css';
 oLink.rel = 'stylesheet';
 oLink.type = 'text/css';
 document.body.appendChild(oLink);
@@ -20,17 +21,37 @@ convertLikes();
 
 function convertLikes(){
     var likes = document.getElementsByClassName('default_message');
+    var more_likes = document.getElementsByClassName('emuEventfad_fan'); // these are the ads on the right of the main timeline
+    //console.log(more_likes);
+    
+    // likes = likes.concat(more_likes); // should meld the main likes with the ad likes
+
     //var butt = like_buttons[0];
-    for (var i = 0; i<likes.length; i++){
-	like = likes[i];
+    convertLikeElements(likes, true);
+    convertLikeElements(more_likes, false);
+}
+
+function convertLikeElements(likeArray, isInnerHTML){
+    //console.log("converting");
+    //console.log(likeArray);
+    for (var i = 0; i<likeArray.length; i++){
+	like = likeArray[i];
 	//like_message = like.getElementsByClassName('default_message')[0];
-	like_message = like.innerHTML;
+	if (isInnerHTML) like_message = like.innerHTML;
+	else like_message = like.text;
+
 	if (like_message == 'Like'){
 	    like.innerHTML = megusta_message;
 	}
 	else if (like_message == 'Unlike'){
 	    like.innerHTML = "<img src='http://i.imgur.com/7Q9Q6.png'> Don't want!";
 	}
-    }
+	else if (like_message == 'Like This Page'){
+	    like.innerHTML = megusta_message + " this page";
+	}
+    }    
 }
 
+function isSecure(){
+    return window.location.protocol == 'https:';
+}
